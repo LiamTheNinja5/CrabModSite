b1ab9196d8931b7c37ca399d662e003d5cad6830
Crispy <frostyid1@gmail.com>
2021-11-07 01:34:28 +1100

    oop x2

--- src/classes/App.js
+++ src/classes/App.js
@@ -34,7 +34,7 @@ class App {
         logger.token('proxy_ip', function getId (req) {
             return req.headers["x-real-ip"]
           })
-        this.app.use(logger(':method > :url :status :res[content-length] | :response-timems | (:proxy_ip)'));
+        this.app.use(logger(':method > :url :status :res[content-length] | :response-time ms | (:proxy_ip)'));
         this.app.use(express.json());
         this.app.use(express.urlencoded({
             extended: true
